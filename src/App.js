420a1e3e04d073d05fcc0dd3d9ea81be70358013
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-01-13 16:45:25 -0600

    App.js has react router dom strcture and a home page route.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+// REACT ROUTER DOM ELEMENTS 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// IMPORTED COMPONENTS 
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        {/* ROUTES HOLDS ALL THE ROUTING DATA  */}
+        <Routes>
+          {/* ROUTE HOLDS DATA OF EACH INDIVIDUAL ROUTE */}
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,10 @@
-import '../CSS/Home'
+import '../CSS/Home.css'
 import React from 'react'
 
 function Home() {
   return (
     <div className='Home'>
-      
+      THIS IS THE HOME PAGE
     </div>
   )
 }
