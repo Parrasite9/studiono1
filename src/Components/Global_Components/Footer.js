7504f9d2f1083387b9202aec7911c83e5085246c
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-01-14 20:05:16 -0600

    Footer section has needed details, not fully CSSd at this time.

--- src/Components/Global_Components/Footer.js
+++ src/Components/Global_Components/Footer.js
@@ -1,9 +1,45 @@
 import '../../CSS/Global_Components/Footer.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div className='Footer'>
+      <div className="logo">
+        STUDIO NO. 1
+      </div>
+
+      <div className="location">
+        <h4 className='footer__header'>Location</h4>
+        <h5>107 W Hornbeck Blvd Fort Stockton, TX 79735</h5>
+      </div>
+
+      <div className="pages">
+          <h4 className='footer__header'>Pages</h4>
+          <Link className="footer-link" to="/">Home</Link>
+          <Link className="footer-link" to="/AboutUs">About Us</Link>
+          <Link className="footer-link" to="/Services">Services</Link>
+          <Link className="footer-link" to="/FAQ">FAQ</Link>
+          <Link className="footer-link" to="/ContactUs">Contact Us</Link>
+      </div>
+
+      <div className="privacy_and_legal">
+          <h4 className='footer__header'>Privacy & Legal</h4>
+          <Link className="footer-link" to="/privacy-policy">Privacy Policy</Link>
+          <Link className="footer-link" to="/terms-of-service">Terms of Service</Link>
+      </div>
+
+      <div className="follow__us">
+        <h4 className='footer__header'>Follow Us!</h4>
+      </div>
+
+      <div className="payment">
+        <h4 className='footer__header'>Acceptable Forms of Payment</h4>
+      </div>
+
+      <div className="copyright">
+        <p>© 2024 Studio No. 1. All Rights Reserved.</p>
+      </div>
       
     </div>
   )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,15 @@ import '../CSS/Home.css'
 import React from 'react'
 import Navbar from './Global_Components/Navbar'
 import Hero from './Hero'
+import Footer from './Global_Components/Footer'
 
 function Home() {
   return (
     <div className='Home'>
       <Navbar />
       <Hero />
-      
+
+      <Footer />
     </div>
   )
 }
