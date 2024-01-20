5c388714c7aa9bd83db1feb7c3b2a25f3a7f6076
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-01-20 14:35:35 -0600

    AboutPage has full text and image. This is basic outline and has not been completed.

--- src/Components/HomePage/AboutPage.js
+++ src/Components/HomePage/AboutPage.js
@@ -2,13 +2,38 @@ import React from 'react'
 import Footer from '../Global_Components/Footer'
 import Navbar from '../Global_Components/Navbar'
 
+import '../../CSS/AboutPage.css'
 function AboutPage() {
   return (
     <div className='AboutPage'>
         <Navbar />
-TEST TEST TEST
+
+        <div className="image__container">
+            <img src="/images/about/group_mobile.jpg" alt="" />
+        </div>
+
+        <div className="about__text">
+
+            <h3>About Studio No. 1</h3>
+
+            <p><strong>Welcome to Studio No. 1,</strong>where timeless beauty meets modern elegance. Nestled in the heart of our vibrant city, we're more than just a beauty salon; we're a sanctuary for those seeking to rejuvenate their spirit and revitalize their look.
+            <br />
+            <br />
+            With over a century of combined experience, our team at Studio No. 1 is a tapestry of expertise, passion, and dedication. We pride ourselves on our rich heritage in the beauty industry, seamlessly blending traditional techniques with contemporary styles to offer you an unparalleled beauty experience.</p>
+
+            <p><strong>Our Services: </strong>At Studio No. 1, we understand that beauty is not one-size-fits-all. That's why we offer a comprehensive range of services tailored to meet the unique needs of each client. Our salon caters to both men and women, providing a wide array of beauty and spa services. Indulge in our expertly delivered massages, where our skilled masseuses and massage therapists work to melt away your stress and tension, leaving you refreshed and revitalized. Treat your hands and feet to a luxurious journey with our manicure and pedicure services, designed to pamper and perfect.
+            <br />
+            <br />
+            For those seeking to elevate their skincare routine, our facials are a must-try, incorporating the finest products and techniques to bring out your skin's natural glow. Our estheticians are masters of their craft, offering personalized services that range from skincare consultations to advanced treatments, ensuring your skin receives the care it deserves.
+            </p>
+
+            <p><strong>Our Commitment: </strong>At Studio No. 1, your satisfaction is our highest priority. We're committed to providing an environment that is not just welcoming, but also invigorating and inspiring. Our salon is a space where creativity flourishes, and individual beauty is celebrated.
+            <br />
+            <br />
+            Join us at Studio No. 1, where every visit is an opportunity to experience the pinnacle of beauty and relaxation. We're not just a salon; we're your partners in the journey towards a more radiant and confident you.</p>
+
+        </div>
         <Footer />
-      
     </div>
   )
 }
--- src/Components/HomePage/AboutUs.js
+++ src/Components/HomePage/AboutUs.js
@@ -13,11 +13,7 @@ function AboutUs() {
         <img src="images/about/group_home.jpg" alt="Salon__img" />
       </div>
 
-      <p>At Studio No. 1, we blend over 100 years of combined expertise in beauty and 
-          wellness to offer you an exceptional experience. Our salon, a haven for both men and 
-          women, specializes in a variety of services. From rejuvenating massages and skincare 
-          treatments by our expert estheticians to luxurious manicures, pedicures, and personalized 
-          beauty services, we cater to all your needs in a welcoming and refined environment.</p>
+      <p>At Studio No. 1, we blend over 100 years of combined expertise in beauty and wellness to offer you an exceptional experience. Our salon, a haven for both men and women, specializes in a variety of services. From rejuvenating massages and skincare treatments by our expert estheticians to luxurious manicures, pedicures, and personalized beauty services, we cater to all your needs in a welcoming and refined environment.</p>
 
       <div className="about__button">
         <Link to="/About-Us"><button className='aboutUs__button'>Read More</button></Link>
