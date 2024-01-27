a73d9c4109e05d086efed876a850afd3ddec02cc
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-01-27 15:52:07 -0600

    ruby has updated services schema

--- src/Components/TeamMemberPage/Prices.js
+++ src/Components/TeamMemberPage/Prices.js
@@ -13,80 +13,21 @@ function Prices() {
       <h2>Prices</h2>
       <p>Prices are Subject to Change</p>
       <div className="pricing__container">
+            {Object.entries(teamMemberData.services)
+                .filter(([key, _]) => !key.endsWith('Price'))
+                .map(([key, value], index) => {
+                    const priceKey = `${key}Price`;
+                    return (
+                        <div key={index} className="service__and__price">
+                            <h4>{value}</h4>
+                            <p>...</p>
+                            <h4>${teamMemberData.services[priceKey]}</h4>
+                        </div>
+                    );
+                })
+            }
+       </div>
 
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.haircut}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.haircutPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.balyage}</h4>
-            <p>........</p>
-            <h4>${teamMemberData.services.womens.balyagePrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.waxing}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.waxingPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.color}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.colorPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.highlights}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.highlightsPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.rootTouchUp}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.rootTouchUpPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.perm}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.permPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.relaxer}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.relaxerPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.whiteout}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.whiteoutPrice}</h4>
-        </div>
-
-        {/* SERVICE  */}
-        <div className="service__and__price">
-            <h4>{teamMemberData.services.womens.haircut}</h4>
-            <p>...</p>
-            <h4>${teamMemberData.services.womens.haircutPrice}</h4>
-        </div>
-
-
-
-      </div>
 
     </div>
   )
