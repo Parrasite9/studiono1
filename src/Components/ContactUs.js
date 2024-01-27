5d084c839af9aa9c57c138b472212b6ebcacc572
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-01-26 21:24:13 -0600

    conditional set up to display contat infomration on desktop and hide on mobile.

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,5 +1,5 @@
 import '../CSS/ContactUs.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Global_Components/Navbar'
 import Footer from './Global_Components/Footer'
 
@@ -35,6 +35,7 @@ function ContactUs() {
   return (
     <div className='ContactUs'>
       <Navbar />
+      <div className="contactUs__container">
       <form className='contact__form'>
         <input type="text" id='name' name='name' placeholder='Name' required/>
 
@@ -42,36 +43,44 @@ function ContactUs() {
 
         <input type="text" id='subject' name='subject' placeholder='Subject' required />
         
-        <textarea name="message" id="message" cols="30" rows="10" placeholder='My service was amazing!!'></textarea>
+        <textarea name="message" id="message" cols="30" rows="10" placeholder='My service was amazing!!' required></textarea>
 
         <button type='submit'>Submit</button>
       </form>
 
-      <div className="contact__info">
-        <h3>Contact Information</h3>
-        <p>We're open for any suggestions or just to have a chat!</p>
+      {isLargeScreen ? (
+        <>
+          <div className="contact__info">
+            <h3>Contact Information</h3>
+            <p>We're open for any suggestions or just to have a chat!</p>
 
-        <div className="icon__and__text">
-          <LocationOnIcon />
-          <p><strong>Address: </strong>107 W Hornbeck Blvd. Fort Stockton, TX 79735</p>
-        </div>
+            <div className="icon__and__text">
+              <LocationOnIcon />
+              <p><strong>Address: </strong>107 W Hornbeck Blvd. Fort Stockton, TX 79735</p>
+            </div>
 
-        <div className="icon__and__text">
-          <LocalPhoneIcon />
-          <p><strong>Phone: </strong>888-888-8888</p>
-        </div>
+            <div className="icon__and__text">
+              <LocalPhoneIcon />
+              <p><strong>Phone: </strong>888-888-8888</p>
+            </div>
 
-        <div className="icon__and__text">
-          <EmailIcon />
-          <p><strong>Email: </strong>Support@StudioOneFs.com</p>
-        </div>
+            <div className="icon__and__text">
+              <EmailIcon />
+              <p><strong>Email: </strong>Support@StudioOneFs.com</p>
+            </div>
 
-        <div className="icon__and__text">
-          <PublicIcon />
-          <p><strong>Website: </strong><a href="StudioOneFs.com">StudioOneFs.com</a></p>
-        </div>
+            <div className="icon__and__text">
+              <PublicIcon />
+              <p><strong>Website: </strong><a href="StudioOneFs.com">StudioOneFs.com</a></p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+
+
       </div>
-
       <Footer />
     </div>
   )
